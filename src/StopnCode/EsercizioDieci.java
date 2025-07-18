d9089d914ba94c6ad915d976cb4056b379f7c69d
Marco Fuchs <furfaro.markus@gmail.com>
2025-07-18 13:12:41 +0200

    First Stop&Code con esercizio Dieci

---
+++ src/StopnCode/EsercizioDieci.java
@@ -0,0 +1,10 @@
+package StopnCode;
+
+public class EsercizioDieci {
+    public static void main(String[] args) {
+        String ciao = "Ciao";
+        String hello = "Hello";
+        ciao = hello;
+        System.out.println(ciao);
+    }
+}
